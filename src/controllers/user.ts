4d56ee456708a0f42b6d323aff0ccadd3a86d4c3
shindev285 <vhcity285@gmail.com>
2024-11-09 08:38:18 +0700

    get token crete acount

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,31 +1,41 @@
-import  dotenv  from 'dotenv';
+import dotenv from "dotenv";
 import UserModel from "../models/UserModel";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+import { getAccessToken } from "../utils/getAccessToken";
 
 dotenv.config();
 const register = async (req: any, res: any) => {
   const body = req.body;
   const { name, email, password } = body;
+
+  
+
   try {
     // email phải là duu nhất
     const user = await UserModel.findOne({ email });
-      if(user){
-        throw new Error(`Email ${email} không tồn tại`);
-
-      }
-    // hassparword 
-      const salt = await bcrypt.genSalt(10);
-      const hashPassword = await bcrypt.hash(password, salt);
-      body.password = hashPassword;
-      const newUser = new UserModel(body);
-      await newUser.save();
-      // //  Xóa password
-      // delete  newUser.password;
-
+    if (user) {
+      throw new Error(`Email ${email} không tồn tại`);
+    }
+    // hassparword
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    body.password = hashPassword;
+    const newUser: any = new UserModel(body);
+    await newUser.save();
+    // //  Xóa password
+    // delete  newUser.password;
+    delete newUser.password;
     // Trả về phản hồi thành công với mã trạng thái 200
     res.status(200).json({
       message: "User created successfully ,register",
-      data: newUser,
+      data: {
+        ...newUser._doc,
+        token: await getAccessToken({
+          _id: newUser._id,
+          email: newUser.email,
+          rule: 1,
+        }),
+      },
     });
   } catch (error: any) {
     // Trả về mã lỗi 404 và phản hồi lỗi
---
+++ src/utils/getAccessToken.ts
@@ -0,0 +1,15 @@
+import  dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import { Types } from 'mongoose';
+
+dotenv.config();
+
+export const getAccessToken =async (payload :{
+  _id: Types.ObjectId;
+  email: string;
+  rule: number;
+
+}) => {
+  const token = jwt.sign(payload,process.env.SECRET_KEY  as string);
+  return token;
+}
